37255dcb1d3452c057afe7b08a62ad5c3037a944
Rohma Wajid Rasul <gusrasuro@student.gu.se>
2026-02-08 16:08:48 +0100

    feat: implemented functionality to mark a task as done (#1)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from task_manager import add_task, list_tasks
+from task_manager import add_task, list_tasks, mark_task_done
 
 def main():
 
@@ -6,10 +6,11 @@ def main():
 
     print("Welcome to Task Manager")
 
-    while choice != "3":
+    while choice != "4":
         print("\n1. Add task")
         print("2. List tasks")
-        print("3. Exit")
+        print("3. Mark task as done")
+        print("4. Exit")
 
         choice = input("Choose: ")
 
@@ -19,6 +20,12 @@ def main():
         elif choice == "2":
             list_tasks()
         elif choice == "3":
+            index = int(input("Which task would you like to mark as done?")) - 1
+            if mark_task_done(index):
+                print("Done!")
+            else:
+                print("Task not found")
+        elif choice == "4":
             print("Gooodbye!")
         else:
             print("Invalid choice")
--- app/task_manager.py
+++ app/task_manager.py
@@ -11,3 +11,10 @@ def list_tasks():
     for i, task in enumerate(tasks):
         status = "Done" if task["done"] else "TODO"
         print(f"{i+1}. [{status}] {task['title']}")
+
+def mark_task_done(index):
+    if 0 <= index < len(tasks):
+        tasks[index]["done"] = True
+        return True
+
+    return False
